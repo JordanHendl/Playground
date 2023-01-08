c8e556bf7287075ae25dca8f90322b0994d68d7e
Jordie <jordiehendl@gmail.com>
2023-01-07 19:26:49 -0600

    begin start of text rendering

---
+++ text_rendering/text_rendering/sdl_text_test.hpp
@@ -0,0 +1,7 @@
+#include "scene_graph/scene.hpp"
+#include <iostream>
+#include <cassert>
+using namespace luna;
+inline auto test_sdl_text() -> void {
+
+}
---
+++ text_rendering/text_rendering/test.cpp
@@ -0,0 +1,10 @@
+#include "text_rendering/sdl_text_test.hpp"
+#include "text_rendering/render_test.hpp"
+#include <iostream>
+#include <cassert>
+using namespace luna;
+auto main() -> int {
+  test_sdl_text();
+  //test_rendering();
+  return 0;
+}
